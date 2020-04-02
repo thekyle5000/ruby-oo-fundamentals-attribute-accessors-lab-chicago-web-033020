1637c81209dbc904573e20a32fa7ac8c54d8f265
Kyle Bradley <kylepersonalmail@gmail.com>
2020-04-02 00:17:16 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,21 +2,14 @@
 require 'pry'
 
 class Cat
-  def initialize(x, y)
-  @x = x     @y = y 
   
-end
+  def initialize(name)
+  @name = name
+  end
   
   attr_accessor :name
-  
   def meow
     puts "meow!"
   end
-  
-  
-end
 
-def initialize(x, y)
-  @x = x     @y = y 
-  
 end
